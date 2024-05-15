ae3abad6aefbe42d0f07ad8260b624f5cefc9d0f
Trishan Wagle <mailtotrishan@gmail.com>
2024-05-15 11:14:39 +0545

    feat: added handlers

--- discord-bot/src/commands/handlers/index.ts
+++ discord-bot/src/commands/handlers/index.ts
@@ -28,17 +28,81 @@ export const handleCreate = async (interaction: ChatInputCommandInteraction) => 
 }
 
 export const handleRegister = async (interaction: ChatInputCommandInteraction) => {
+    const name = interaction.options.get("name")?.value as string;
+    const email = interaction.options.get("email")?.value as string;
+    const password = interaction.options.get("password")?.value as string;
 
+    try {
+        const response = await axios.post(API_ENDPOINTS["REGISTER_URL"], {
+            name,
+            email,
+            password
+        })
+        return interaction.reply({
+            content: `
+            @${interaction.user.username}\nSuccessfully registered!\n\nHere's your details: \`\`\`\n${JSON.stringify(response?.data?.data)}\`\`\``,
+        })
+    } catch (err: any) {
+        return interaction.reply({
+            content: err?.response?.data?.message || "Failed to register. Please try again later.",
+        })
+    }
 }
 
 export const handleLogin = async (interaction: ChatInputCommandInteraction) => {
+    const email = interaction.options.get("email")?.value as string;
+    const password = interaction.options.get("password")?.value as string;
 
+    try {
+        const response = await axios.post(API_ENDPOINTS["LOGIN_URL"], {
+            email,
+            password
+        })
+        return interaction.reply({
+            content: `
+            @${interaction.user.username}\nSuccessfully logged in !\n\nHere's your **access token**: \`\`\`\n${response?.data?.data?.accessToken}\`\`\`\nHere's your ** refresh token **: \`\`\`\n${response?.data?.data?.refreshToken}\`\`\``,
+        })
+    } catch (err: any) {
+        return interaction.reply({
+            content: err?.response?.data?.message || "Failed to login. Please try again later.",
+        })
+    }
 }
 
 export const handleGetProfile = async (interaction: ChatInputCommandInteraction) => {
+    const accessToken = interaction.options.get("access-token")?.value as string;
 
+    try {
+        const response = await axios.get(API_ENDPOINTS["GET_ME"], {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        return interaction.reply({
+            content: `Here's your profile details: \`\`\`\n${JSON.stringify(response?.data?.data)}\`\`\``,
+        })
+    } catch (err: any) {
+        return interaction.reply({
+            content: err?.response?.data?.message || "Failed to login. Please try again later.",
+        })
+    }
 }
 
 export const handleRefresh = async (interaction: ChatInputCommandInteraction) => {
+    const refreshToken = interaction.options.get("refresh-token")?.value as string;
 
+    try {
+        const response = await axios.get(API_ENDPOINTS["REFRESH"], {
+            headers: {
+                "x-refresh-token": refreshToken
+            }
+        })
+        return interaction.reply({
+            content: `Here's your new access token: \`\`\`\n${response?.data?.data?.accessToken}\`\`\``,
+        })
+    } catch (err: any) {
+        return interaction.reply({
+            content: err?.response?.data?.message || "Failed to refresh token. Please try again later.",
+        })
+    }
 }
